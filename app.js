8733af1987cf614032f432ed583785b47cc57216
david <dspiller321@gmail.com>
2026-07-30 17:45:56 +0300

    add the server

---
+++ app.js
@@ -0,0 +1,24 @@
+import router from "./router/router.js";
+import {dotenv} from "dotenv/config"
+import supabase from "./db/database.js";
+import express from "express"
+
+
+const app = express()
+
+app.use(express.json())
+
+app.use(router)
+
+app.use((req,res,next) =>{
+    const err = new Error("route not found")
+    const status = 404
+    next(err)
+})
+
+app.use((err,req,res,next) =>{
+    res.status(err.status || 500)
+    .json({error:err.message})
+})
+
+app.listen(3000,() =>console.log("server runnig on port 3000"))
