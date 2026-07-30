93d021d9cc0ff6fbd76260167cb0da390ad52faa
david <dspiller321@gmail.com>
2026-07-30 17:37:50 +0300

    add function of repoBenifsoldier

---
+++ repository/repoBenifSolder.js
@@ -0,0 +1,71 @@
+import supabase from "../db/database.js"
+export async function checkSoliderBenifRepo(soldierId){
+    try{
+    const find = await supabase
+        .from('benefitsoldier')
+        .select('*')
+        .eq('soldierId',soldierId)
+    
+    }
+    catch(err){
+        const err = new Error("internal server error")
+        throw(err)
+    }
+}
+export async function createBenefSoldierRepo(soldierId,unit,currentBenefitType,BenefitPeriod){
+    try{
+    const insert = await supabase
+        .from('benefitsoldier')
+        .insert({soldierId:soldierId,unit:unit,currentBenefitType:currentBenefitType,history:BenefitPeriod})
+        .select()
+    
+    }
+    catch(err){
+        const err = new Error("internal server error")
+        throw(err)
+    }
+}
+
+export async function updateCurrentBenefit(benefitType,soldierId){
+    try{
+        await supabase
+        .from('benefitsoldier')
+        .update({currentBenefitType:benefitType})
+        .eq('soldierId',soldierId)
+    }
+    catch(err){
+        const err = new Error("internal server error")
+        throw(err)
+    }
+}
+
+export async function getHistoryRepo(soldierId){
+    try{
+        const history = await supabase
+        .from('benefitsoldier')
+        .select('history')
+        .eq('soldierId',soldierId);
+        
+     
+    }
+    catch(err){
+        const err = new Error("internal server error")
+        throw(err)
+    }
+}
+
+export async function updateHistory(getHistory,soldierId){
+    try{
+        await supabase
+        .from('benefitsoldier')
+        .update({history:getHistory})
+        .eq('soldierId',soldierId)
+    }
+    catch(err){
+        const err = new Error("internal server error")
+        throw(err)
+    }
+}
+
+
+    
